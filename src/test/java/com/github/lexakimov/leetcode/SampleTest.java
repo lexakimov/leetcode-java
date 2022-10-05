145de66ae37a6a54be6a919f90ac1529a5d1c5ee
Alexey Akimov <lex.akimov23@gmail.com>
2022-10-05 21:44:40 +0700

    initial commit

---
+++ src/test/java/com/github/lexakimov/leetcode/SampleTest.java
@@ -0,0 +1,20 @@
+package com.github.lexakimov.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author akimov
+ * created at 05.10.2022 21:41
+ */
+public class SampleTest {
+    @Test
+    void name() {
+        Assertions.assertTrue(true);
+    }
+    
+    @Test
+    void name2() {
+        Assertions.fail();
+    }
+}
